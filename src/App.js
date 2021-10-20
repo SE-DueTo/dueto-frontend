1b6fef6dbfe1657e3f66fc2e9886f24b874016a5
akkjon <akkjon@outlook.de>
2021-10-20 15:24:46 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function Title() {
+  return <div>
+      <h1>DueTo</h1>
+      <h3>Comming Soon</h3>
+    </div>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title/>
+      </header>
+    </div>
+  );
+}
+
+export default App;

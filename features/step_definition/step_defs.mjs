8f625ca294cf192896aefef6fe3be9cfdb3bc8c4
akkjon <akkjon@outlook.de>
2021-11-02 13:19:04 +0100

    webbrowser start on each cucumber test

--- features/step_definition/step_defs.mjs
+++ features/step_definition/step_defs.mjs
@@ -1,4 +1,4 @@
-import { Given, When, Then, After } from '@cucumber/cucumber'
+import { Given, When, Then, After, Before } from '@cucumber/cucumber'
 import { strict as assert } from 'assert'
 import webdriver, { Builder } from 'selenium-webdriver'
 import { Options } from 'selenium-webdriver/edge.js'
@@ -8,12 +8,12 @@ let edgeOptions = new Options();
 const rootURL = "http://localhost:3000";
 let driver;
 
-Given("the browser loads", async ()=>{
-     driver = await new Builder()
+Before(async ()=>{
+    driver = await new Builder()
         .setEdgeOptions(edgeOptions)
         .forBrowser('MicrosoftEdge')
         .build();
-});
+})
 
 Given("the website is {string}", async (url) => {
     return await driver.get(rootURL + url)

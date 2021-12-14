0d4719b7f08198e0d254eec70e0790c6c533711b
BuildTools <marco.schmidt03@gmail.com>
2021-12-14 21:43:06 +0100

    added Transaction Type  to Types

--- src/TransactionTable.tsx
+++ src/TransactionTable.tsx
@@ -1,4 +1,54 @@
-import { Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow } from '@mui/material'
+import { Avatar, Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow } from '@mui/material'
+import { type } from 'os';
+import { useContext } from 'react';
+import { GroupUserdataContext } from './contexts';
+import { Group, GroupType, PaymentMethods, Transaction, User } from './Types';
+
+const user:User = {
+  userId: 0,
+  username: "Username",
+  email: "user@example.com",
+  avatarUrl: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Hauskatze_langhaar.jpg"
+}
+const user2:User = {
+  userId: 1,
+  username: "Username2",
+  email: "user2@example.com",
+  avatarUrl: null
+}
+
+const groups:Group[] = [
+  {
+      groupId: 0,
+      groupname: "Test_0",
+      type: GroupType.NORMAL,
+      users: [user]
+  }, {
+      groupId: 1,
+      groupname: "Bla",
+      type: GroupType.SPONTANEOUS,
+      users: [user, user2]
+  }, 
+]
+
+const transactions:Transaction[] = [
+  {
+    transactor: user,
+    group: groups[0],
+    amount: 19.99,
+    paymentMethod: PaymentMethods.CASH,
+    purpose: "Einkaufen Rewe",
+    date: "14.12.2021"
+  }, 
+  {
+    transactor: user2,
+    group: groups[0],
+    amount: 19.99,
+    paymentMethod: PaymentMethods.CASH,
+    purpose: "Einkaufen Rewe",
+    date: "14.12.2021"
+  }, 
+]
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -11,7 +61,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     },
   }));
   
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
     '&:nth-of-type(odd)': {
       backgroundColor: theme.palette.action.hover,
     },
@@ -19,25 +69,32 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     '&:last-child td, &:last-child th': {
       border: 0,
     },
-  }));
+ }));
   
-  function createData(
+ function createData(
     avatarUrl: string | null,
     name: string,
     amount: number,
     purpose: string,
-    paymentMethod: string,
+    paymentMethod: PaymentMethods,
     dateOfPayment: string,
   ) {
     return { avatarUrl, name, amount, purpose, paymentMethod, dateOfPayment };
-  }
+}
   
-  const rows = [ 
-    createData('Hallo', 'Hans', 29.99, 'Essen gehen', 'Bar', '14.12.2021'),
-    createData('Test', 'Gruppe', 237, 'Urlaub in Mexico City', 'Card', '08.12.2021')
-  ];
 
-export default function TransactionTable() {
+
+type TransactionTableProps ={
+
+}
+const TransactionTable:React.FC<TransactionTableProps> = () => {
+
+  const rows = [ 
+     createData('Hallo', 'Hans', 29.99, 'Essen gehen', PaymentMethods.CASH, '14.12.2021'),
+     createData('Test', 'Gruppe', 237, 'Urlaub in Mexico City', PaymentMethods.CREDITCARD, '08.12.2021'),
+  ];
+  
+  const groupUserdata = useContext(GroupUserdataContext)
 
     return (
         <TableContainer component={Paper}>
@@ -55,7 +112,8 @@ export default function TransactionTable() {
               {rows.map((row) => (
                 <StyledTableRow key={row.avatarUrl}>
                   <StyledTableCell component="th" scope="row">
-                    {row.avatarUrl + ' ' + row.name}
+                    <Avatar src={groupUserdata.user?.avatarUrl ?? undefined}>{groupUserdata.user?.username[0]}</Avatar>
+                    {row.name}
                   </StyledTableCell>
                   <StyledTableCell align="right">{row.amount} €</StyledTableCell>
                   <StyledTableCell align="right">{row.purpose}</StyledTableCell>
@@ -68,3 +126,5 @@ export default function TransactionTable() {
         </TableContainer>
       );
 }
+
+export default TransactionTable
